5b003db5a2667e5b82eaa6fecfad6c71e7b9ad24
Dihfahsih1 <dihfahsihm@gmail.com>
2020-01-24 14:52:34 +0300

    update form.py with new changes

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,58 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from dashboard.models import User
 
-class RegisterForm(UserCreationForm):
-	email = forms.EmailField()
-	class Meta:
-		model = User
-		fields = ['username', 'email', 'password1', 'password2']
+class RegisterForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='Password confirmation',
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+
+    class Meta:
+        model = User
+        fields = ['full_name','username','Role']
+        # 'roles'
+
+        labels = {
+            'full_name': 'Name',
+        }
+
+        error_messages = {
+            'full_name': {
+                'max_length': "Name can only be 25 characters in length"
+            }
+        }
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+'''class UserEmailForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if not email:
+            return email
+        try:
+            user = User.objects.get(email=email)
+            if user.email == self.instance.email:
+                return email
+        except User.DoesNotExist:
+            return email
+
+        raise ValidationError(_("This email is already used."))'''
+
