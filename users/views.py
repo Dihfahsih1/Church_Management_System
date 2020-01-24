ce642930333276010fd93b8a85fb3f0921c1867c
Dihfahsih1 <dihfahsihm@gmail.com>
2020-01-24 11:23:07 +0300

    add module for creating new system users by the admin

--- users/views.py
+++ users/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib import messages
 from .forms import RegisterForm
+from django.contrib.auth.models import User
 def register(request):
+	users=User.objects.all()
 	if request.method == 'POST':
 		form = RegisterForm(request.POST)
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
 			messages.success(request, f'Account has been created successfully!, you can now login')
-			return redirect('login')
+			return redirect('register')
 	else:
 		form = RegisterForm()
-	return render(request, 'users/home/register.html', {'form': form})
+	return render(request, 'users/home/register.html', {'form': form,'users':users})
 
+def delete_user(request,pk):
+    user= get_object_or_404(User, id=pk)
+    if request.method == "GET":
+        user.delete()
+        messages.success(request, "user successfully deleted!")
+        return redirect("register")
+    context= {'user': user}
+    return render(request, 'users/home/delete_user.html', context)
