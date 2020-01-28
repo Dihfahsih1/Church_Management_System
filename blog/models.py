2494d9c14274bd8cab1d918fabdb6eda1777ca96
Dihfahsih1 <dihfahsihm@gmail.com>
2020-01-28 16:12:19 +0300

    migrations of a specific app using python manage.py makemigrations appname

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+from dashboard.models import User
+from django.urls import reverse
+
+class posts(models.Model):
+    Title = models.CharField(max_length=100)
+    Post_content = models.TextField()
+    Date_posted = models.DateTimeField(default=timezone.now)#auto_now_add=True
+    Name = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
