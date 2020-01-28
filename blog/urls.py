0e0e37162a74e7dd16b72c6157145d9a7c39ae50
Dihfahsih1 <dihfahsihm@gmail.com>
2020-01-28 17:11:56 +0300

    did almost nothing today

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
+    path('blog-home', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
