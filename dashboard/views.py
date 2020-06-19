58a2aea75a1a905848132c787455547799d2a09c
Dihfahsih1 <dihfahsihm@gmail.com>
2020-06-19 17:17:30 +0300

    now the confirmation link also works fine

--- dashboard/views.py
+++ dashboard/views.py
@@ -29,7 +29,6 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .tokens import account_activation_token  
 from django.core.mail import EmailMessage
-import codecs
 
 # #######################################===>BEGINNING OF THEME MODULE<===############################################
 
