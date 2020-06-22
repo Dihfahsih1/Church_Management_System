645124f0309b5027869d4dafe03b568d1a3c5c05
Dihfahsih1 <dihfahsihm@gmail.com>
2020-06-22 21:58:23 +0300

    delete scripts that are not necessary

--- dashboard/connecting.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(
-	'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'MugoYA23?',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        ) 
-print('connected successfully')
