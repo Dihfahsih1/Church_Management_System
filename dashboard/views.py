5a16aa6e5ce16626f250db16d93c5930c3c9df2e
Dihfahsih1 <dihfahsihm@gmail.com>
2020-09-22 09:48:03 +0300

    changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -25,7 +25,6 @@ import urllib
 from django.contrib.sites.shortcuts import get_current_site  
 from django.utils.encoding import force_bytes, force_text  
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
-from django.template.loader import render_to_string
 from .tokens import account_activation_token  
 from django.core.mail import EmailMessage
 from background_task import background
