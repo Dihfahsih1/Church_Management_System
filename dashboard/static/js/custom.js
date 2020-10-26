f0ab88bb654eec3c57d497bf555ba9209fe8283b
Canon Innovations Ltd <dihfahsihm@gmail.com>
2020-10-26 23:40:05 +0300

    insert resources for the datapicker

--- dashboard/static/js/custom.js
+++ dashboard/static/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $(".registration_date").datepicker({changeYear: true, changeMonth: true});
-    $(".datetimeinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
+    
 
 });
 
