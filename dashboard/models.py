79c14f59a8a4dc627f3c8686f59a06157797c326
Canon Innovations Ltd <dihfahsihm@gmail.com>
2020-10-27 23:16:51 +0300

    add more choices on the model

--- dashboard/models.py
+++ dashboard/models.py
@@ -9,11 +9,10 @@ from django.forms.fields import DateField
 from django.contrib.admin.widgets import AdminDateWidget
 from django.contrib.auth.models import PermissionsMixin
 
-services = (
-         ('Unspecified Service','Unspecified Service'),
+services = (('Sunday First Service','Sunday First Service'),('Sunday Second Service','Sunday Second Service'),('Sunday Third Service','Sunday Third Service'), 
+         ('All Sunday Services','All Sunday Services'),('Unspecified Service','Unspecified Service'),
         ('Home Cell Service','Home Cell Service'),('Youth Service','Youth Service'),('Wednesday Service','Wednesday Service'),
-        ('Bible Study Service','Bible Study Service'),('Friday Overnight','Friday Overnight'),('Sunday First Service','Sunday First Service'),
-        ('Sunday Second Service','Sunday Second Service'),('Sunday Third Service','Sunday Third Service'),
+        ('Bible Study Service','Bible Study Service'),('Friday Overnight','Friday Overnight')
         )
 
 ministries=(
