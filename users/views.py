bd442642940e0e6a1eb7cf6cf5ffea14d4d12341
Dihfahsih1 <dihfahsihm@gmail.com>
2021-03-02 18:38:31 +0300

    more changes

--- users/views.py
+++ users/views.py
@@ -105,7 +105,8 @@ def MemberAccountRegister(request):
     return render(request, 'users/home/membershipaccount.html', {'form': form,'members':members})
 def member_profile(request):
     current_user = request.user.username
-    context={'current_user':current_user}
+    member = Members.objects.get(created_by=request.user)
+    context={'member':member, 'current_user':current_user}
     return render(request,'home/profile.html',context)
 
 @login_required
