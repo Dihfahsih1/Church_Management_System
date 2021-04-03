913b588cb9903a923867eef6a7039b1ec937a9f4
Dihfahsih1 <dihfahsihm@gmail.com>
2021-04-03 22:27:49 +0300

    changes

--- accountant/urls.py
+++ accountant/urls.py
@@ -35,15 +35,11 @@ urlpatterns = [
     
     ###################PASSWORD RESETING AND CHANGE ###################
     url(r'^password/change/', user_views.UserPasswordChangeView.as_view(), name='changing-password'),
-    
     path('Login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     # path('Logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    
     url(r'^logout/', user_views.logout_request, name='logout'),
-    
     url(r'^Password-Reset/', auth_views.PasswordResetView.as_view(template_name='users/home/password_reset.html'),
         name='password_reset'),
-    
     url(r'^Done/Password-Reset', auth_views.PasswordResetDoneView.as_view(template_name='users/home/password_reset_done.html'),
         name='password_reset_done'),
     
