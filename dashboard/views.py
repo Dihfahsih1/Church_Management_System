8a3ee9258ae314ca0e2497b50bc8fb6b3df74fbe
ntcababp <dihfahsihm@gmail.com>
2022-10-27 09:33:14 +0300

    remove redundant comments

--- dashboard/views.py
+++ dashboard/views.py
@@ -307,22 +307,7 @@ def Online_Registration(request):
                     return redirect('member_profile')
                 except BadHeaderError:
                     return HttpResponse('Make sure you enter correct info.')
-            # current_site = get_current_site(request)  
-            # mail_subject = 'Activate your account.'  
-            # message = render_to_string('acc_active_email.html', {  
-            #     'member': member,  
-            #     'domain': current_site.domain,  
-            #     'uid': urlsafe_base64_encode(force_bytes(member.pk)),  
-            #     'token': account_activation_token.make_token(member),  
-            #     }) 
-            # to_email = form.cleaned_data.get('Email')  
-            # email = EmailMessage(  
-            #     mail_subject, message, to=[to_email]  
-            # )  
-            # email.send()
-            #context={'member':member}
-            #return render(request, 'activation_email_sent.html', context) 
-
+           
         else:
             form_errors=form.errors
             context={'form':form,'form_errors':form_errors}
@@ -3404,8 +3389,7 @@ def lwakiolimulamu_detail(request, pk):
     
     
 ############# Blog Module ##################
-def add_blogpost(request):
-    #user = Profile.objects.get(user=request.user)
+def add_blogpost(request): 
     forms = BlogForm(request.POST or None)
     if request.method == 'POST':
         if forms.is_valid():
