600e999c2c9715d327a56bf4dd912fd1c1fe20f6
dev <dihfahsihm@gmail.com>
2023-03-10 16:40:54 +0300

    neglect the feb logic that was causing errors

--- dashboard/archivetransactions.py
+++ dashboard/archivetransactions.py
@@ -1,29 +1,31 @@
 from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 from dashboard.models import Expenditures, Revenues
 
 def archive_transactions():
-    today = datetime.now().date()
-    last_month = today - relativedelta(months=1)
-    start_date = last_month.replace(day=1)
-    end_date = last_month.replace(day=1) + relativedelta(months=1) - relativedelta(days=1)
-    # Filter expenditures for the previous month
-    expenditures = Expenditures.objects.filter(
-        Date__month__gte=start_date.month,
-        Date__year__lte=end_date.year,
-        Archived_Status='NOT-ARCHIVED'
-    )
-    for expenditure in expenditures:
-        expenditure.Archived_Status = 'ARCHIVED'
-        expenditure.save()
-    
-    # Filter revenues for the previous month
-    revenues = Revenues.objects.filter(
-        Date__month__gte=start_date.month,
-        Date__year__lte=start_date.year,
-        Archived_Status='NOT-ARCHIVED'
-    )
-    for revenue in revenues:
-        revenue.Archived_Status = 'ARCHIVED'
-        revenue.save()
+    try:
+        today = datetime.now().date()
+        last_month = today - timedelta(days=30)
+        start_date = last_month.replace(day=1)
+        end_date = (start_date + timedelta(days=32)).replace(day=1) - timedelta(days=1)
+
+        # Filter expenditures for the previous month
+        expenditures = Expenditures.objects.filter(
+            Date__range=(start_date, end_date),
+            Archived_Status='NOT-ARCHIVED'
+        )
+        for expenditure in expenditures:
+            expenditure.Archived_Status = 'ARCHIVED'
+            expenditure.save()
+
+        # Filter revenues for the previous month
+        revenues = Revenues.objects.filter(
+            Date__range=(start_date, end_date),
+            Archived_Status='NOT-ARCHIVED'
+        )
+        for revenue in revenues:
+            revenue.Archived_Status = 'ARCHIVED'
+            revenue.save()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
